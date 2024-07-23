de31e4f8f4e3074ef7e1cc3bb0704d8fb4eb968c
SumairaRasheed <126061302+SumairaRasheed@users.noreply.github.com>
2024-07-23 22:22:52 +0500

    Delete src/Fields/customFileUpload.ts

--- src/Fields/customFileUpload.ts
+++
@@ -1,9 +0,0 @@
-// import { Field } from "payload/types";
-
-// export const customFileUpload: Field = {
-//   name: "fileUpload",
-//   type: "upload",
-//   relationTo: "media",
-//   required: false,
-//   label: "File Upload",
-// };
